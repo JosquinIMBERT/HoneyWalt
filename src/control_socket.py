0d05c5f19926d971aaf0d5fc1d63f4be2ff05fa4
Josquin IMBERT <josquin.imbert@etu.univ-grenoble-alpes.fr>
2022-07-25 11:14:08 +0200

    Using AF_VSOCK

--- src/control_socket.py
+++ src/control_socket.py
@@ -9,63 +9,65 @@ from utils import *
 
 class ControlSocket:
 	def __init__(self, phase):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.connect((glob.CONTROL_IP, glob.CONTROL_PORT))
-		self.sock = self.socket.makefile(mode="rw")
+		self.socket = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
+		self.socket.bind((socket.VMADDR_CID_HOST, glob.CONTROL_PORT))
+		self.socket.settimeout(90) # Timeout for VM connection is 1m30s
+		self.socket.listen(1)
 		self.phase = phase
 
 	def initiate(self, backends=[], usernames=[], passwords=[], images=[]):
 		print("Waiting boot.")
-		if not self.wait_confirm(timeout=90):
-			eprint("ControlSocket.initiate: error: VM failed to boot")
-		
-		print("Send phase.")
-		self.send(str(self.phase))
-		
-		if self.phase == 1:
-			print("Send images.")
-			self.send_elems(images)
-			print("Wait confirm.")
-			if self.wait_confirm():
-				# Sending the users to be added to the images
-				# This will allow cowrie to connect (and will
-				# allow brut force from a node to another)
-				print("Send usernames.")
-				self.send_elems(usernames)
-				print("Send passwords.")
-				self.send_elems(passwords)
-				print("Send backends.")
-				self.send_elems(backends)
-			else:
-				eprint("ControlSocket.initiate: error: failed to download WalT images on the VM")
-			print("Receive IPs.")
-			return self.recv_elems() # Returning backends IPs
-		return None
+
+		try:
+			conn, addr = self.socket.accept()
+		except socket.timeout:
+			eprint("ControlSocket.initiate: error: it seems like the VM failed to boot.")
+		except:
+			eprint("ControlSocket.initiate: error: unknown error occured when waiting for the VM")
+		else:
+			print("Send phase.")
+			self.send(str(self.phase))
+			
+			if self.phase == 1:
+				print("Send images.")
+				self.send_elems(images)
+				print("Wait confirm.")
+				if self.wait_confirm():
+					# Sending the users to be added to the images
+					# This will allow cowrie to connect (and will
+					# allow brut force from a node to another)
+					print("Send usernames.")
+					self.send_elems(usernames)
+					print("Send passwords.")
+					self.send_elems(passwords)
+					print("Send backends.")
+					self.send_elems(backends)
+				else:
+					eprint("ControlSocket.initiate: error: failed to download WalT images on the VM")
+				print("Receive IPs.")
+				return self.recv_elems() # Returning backends IPs
+			return None
 
 	def ask_reboot(self, backend):
 		self.send("reboot:"+backend)
 		return self.wait_confirm()
 
 	def wait_confirm(self, timeout=30):
-		res = self.recv(max_iter=timeout)
+		res = self.recv(timeout=timeout)
 		return res[0] == "1"
 
 	def send(self, string):
-		self.sock.write(string+"\n")
+		self.socket.send(string+"\n")
 
 	def recv(self, max_iter=30):
-		res = ""
-		i = 0
-		while i<max_iter:
-			i+=1
-			res = self.sock.readline()
-			if not res:
-				time.sleep(1)
-				continue
-			break
-		if i >= max_iter:
-			eprint("ControlSocket:recv: error: reached max_iter")
-		return res
+		try:
+			res = self.socket.recv(2048)
+		except socket.timeout:
+			eprint("ControlSocket.recv: error: reached timeout")
+		except:
+			eprint("ControlSocket.recv: error: an unknown error occured")
+		else:
+			return res
 
 	def send_elems(self, elems, sep=" "):
 		str_elems = ""
